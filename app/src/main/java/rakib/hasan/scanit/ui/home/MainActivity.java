f669d82d1272c69009e9d575b60956fb3ac0b6db
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-05 00:23:20 +0600

    Initial commit

---
+++ app/src/main/java/rakib/hasan/scanit/ui/home/MainActivity.java
@@ -0,0 +1,18 @@
+package rakib.hasan.scanit.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rakib.hasan.scanit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
