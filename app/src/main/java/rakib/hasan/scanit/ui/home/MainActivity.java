5452c8d5b10c8da221d603bca83384ea4222ce58
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-05 11:34:03 +0600

    Xml design update

--- app/src/main/java/rakib/hasan/scanit/ui/home/MainActivity.java
+++ app/src/main/java/rakib/hasan/scanit/ui/home/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import rakib.hasan.scanit.R;
+import rakib.hasan.scanit.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
 
     }
 
