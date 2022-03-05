746ae4cdd1df389ab568f102fc294513df5ca56f
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-05 12:06:03 +0600

    Camera permission added!

--- app/src/main/java/rakib/hasan/scanit/ui/home/ScannerActivity.java
+++ app/src/main/java/rakib/hasan/scanit/ui/home/ScannerActivity.java
@@ -8,6 +8,8 @@ import rakib.hasan.scanit.R;
 
 public class ScannerActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
