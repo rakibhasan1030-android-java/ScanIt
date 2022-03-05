1dcbc4c8639789871dee7d6a715bd04482d3c092
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-05 13:17:09 +0600

    Update!

--- app/src/main/java/rakib/hasan/scanit/ui/home/MainActivity.java
+++ app/src/main/java/rakib/hasan/scanit/ui/home/MainActivity.java
@@ -1,26 +1,76 @@
 package rakib.hasan.scanit.ui.home;
 
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-import rakib.hasan.scanit.R;
 import rakib.hasan.scanit.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.scanBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), ScannerActivity.class)));
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+
+        binding.scanBtn.setOnClickListener(v -> {
+            ScanOptions options = new ScanOptions();
+            options.setTorchEnabled(true);
+            options.setBarcodeImageEnabled(true);
+            options.setBeepEnabled(true);
+            options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES);
+            options.setPrompt("Scan a barcode");
+            options.setCameraId(0);  // Use a specific camera of the device
+            options.setBeepEnabled(false);
+            options.setBarcodeImageEnabled(true);
+            barcodeLauncher.launch(options);
+        });
 
     }
+
+
+    // Register the launcher and result handler
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if (result.getContents() == null) {
+                    Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            });
 
 }
--- app/src/main/java/rakib/hasan/scanit/ui/home/ScannerActivity.java
+++ app/src/main/java/rakib/hasan/scanit/ui/home/ScannerActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.BarcodeView;
+
 import rakib.hasan.scanit.R;
 
-public class ScannerActivity extends AppCompatActivity {
-
+public class ScannerActivity extends AppCompatActivity{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
-
     }
+
 }
